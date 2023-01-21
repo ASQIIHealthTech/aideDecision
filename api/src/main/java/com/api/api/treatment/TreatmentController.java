1b62a10e818a135b5c11ff2887c29912c9c34a35
= <=>
2023-01-21 22:06:57 +0100

    added pec and treatment

--- api/src/main/java/com/api/api/treatment/TreatmentController.java
+++ api/src/main/java/com/api/api/treatment/TreatmentController.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/api/treatment")
 public class TreatmentController {
