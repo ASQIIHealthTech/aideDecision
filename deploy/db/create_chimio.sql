2d860dd9bd19458ac22010fb8cc36de8c25f5d07
= <=>
2023-01-24 16:58:57 +0100

    added sql scripts

---
+++ deploy/db/create_chimio.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Chimiothrapie (
+    `id_pec` INT,
+    `type_histo` VARCHAR(15) CHARACTER SET utf8,
+    `clairance` VARCHAR(5) CHARACTER SET utf8,
+    `audiometrie` VARCHAR(25) CHARACTER SET utf8,
+    `egfr` VARCHAR(7) CHARACTER SET utf8,
+    `alk` VARCHAR(7) CHARACTER SET utf8,
+    `braf` VARCHAR(7) CHARACTER SET utf8,
+    `ros1` VARCHAR(7) CHARACTER SET utf8,
+    `pdl1` VARCHAR(14) CHARACTER SET utf8,
+    `ps` VARCHAR(3) CHARACTER SET utf8,
+    `tabac` VARCHAR(10) CHARACTER SET utf8,
+    `Protocole` VARCHAR(48) CHARACTER SET utf8,
+    `id_protocole` INT
+);
